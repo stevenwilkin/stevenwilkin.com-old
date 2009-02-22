de2228fe9a2fe4e8634b01af9317d2db47a02dc9
Steven Wilkin <stevenwilkin@gmail.com>
2009-02-22 18:41:12 +0000

    nav active state. short bio, including age

--- stevenwilkin.com.rb
+++ stevenwilkin.com.rb
@@ -5,9 +5,17 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
+	def age
+		dob = Date::civil(1980, 1, 3)
+		age = Date.today.year - dob.year
+		age -= (Date.today < Date::civil(Date.today.year, dob.month, dob.day)) ? 1 : 0
+	end
+	@id_for_body = 'home'
+	@age = age
 	haml :index
 end
 
 get '/projects' do
+	@id_for_body = 'projects'
 	haml :projects
 end
