c78369262bba23ab535e8df40db4c173f130a98a
Steven Wilkin <stevenwilkin@gmail.com>
2009-08-28 10:58:53 +0100

    initialising google analytics via jQuery

--- public/js/stevenwilkin.js
+++ public/js/stevenwilkin.js
@@ -1,3 +1,14 @@
+function initAnalytics(){
+	var accountId = 'UA-2764682-5';
+	jQuery.getScript('http://www.google-analytics.com/ga.js', function(){
+		try {
+			var pageTracker = _gat._getTracker(accountId);
+			pageTracker._trackPageview();
+		} catch(err) {}
+	});
+}
+
 $(document).ready(function(){
+	initAnalytics();
 	$('#contact p a').attr('href', 'mailto:steve@stevenwilkin.com');
 });
